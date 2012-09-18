e4aeed0d89443631133e568a5bf01ff3606754ff
pad <pad@fb.com>
2012-09-17 17:39:14 -0700

    * codegraph.c: for codegraph ...

---
+++ codegraph.c
@@ -0,0 +1,30 @@
+// this is for codegraph
+
+
+// see usys.S for the actually implementation of those
+// functions
+void fork() { }
+void exit() { }
+void wait() { }
+void pipe() { }
+void read() { }
+void write() { }
+void close() { }
+void kill() { }
+void exec() { }
+void open() { }
+void mknod() { }
+void unlink() { }
+void fstat() { }
+void link() { }
+void mkdir() { }
+void chdir() { }
+void dup() { }
+void getpid() { }
+void sbrk() { }
+void sleep() { }
+
+// builtins for regular programs, ex scripts/mkfs.c
+void perror() { }
+void assert() { }
+
